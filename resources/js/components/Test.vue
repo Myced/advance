841acfea8ce9318b6559464e2067351646a9e029
tnced <tncedric@gmail.com>
2022-02-10 09:22:08 +0100

    Installed laravel

---
+++ resources/js/components/Test.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        This is test template 
+    </div>
+</template>
+
+<script>
+export default {
+    mounted(){
+        console.log("this componentn is mounted");
+    }
+}
+</script>
